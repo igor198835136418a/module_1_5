b9c0e1a2372a926bfb8ca52781255ea4cb75da1d
igor19881 <34863818+igor198835136418a@users.noreply.github.com>
2024-12-20 12:51:24 +0700

    Add files via upload

---
+++ Неизменяемые и изменяемые объекты. Кортежи и списки/module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var =  (1, 2, 3, "a", 'b', "c")
+print(immutable_var)
+#immutable_var [0] = 5 кортеж не поддерживает обращение по элементам!
+mutable_list = [1, 2, 3, 'q', 'w', 'e']
+mutable_list[1] = "Abakan"
+print(mutable_list)
